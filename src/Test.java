f70b8ea958291cf86eb1977f7dc455fd6931bd65
uday kiran <uday.marikanti86@gamil.com>
2024-02-23 15:36:30 +0530

    add

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,9 @@
 public class Test {
     public static void main(String[] args){
         LoginProcess loginProcess=new LoginProcess("vipteam","vip");
-       // loginProcess.login();
-        OrderProcess orderProcess = new OrderProcess(3,"birani","Chicken65");
-        System.out.println(orderProcess.processOrderTest(loginProcess));
-
+        OrderProcess orderProcess = new OrderProcess(2,"birani","Chicken65");
+         orderProcess.processOrderTest(loginProcess);
         Coupons coupons = new Coupons();
-        coupons.amount=600;
         coupons.Couponsprocess(orderProcess);
 
 
