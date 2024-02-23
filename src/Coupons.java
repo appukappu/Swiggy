ab6eda4d70f9d4e3cb9177f125c35d8bb034d257
venkatreddy <mvsateeshqa@gmail.com>
2024-02-23 14:43:16 +0530

    coupans

--- src/Coupons.java
+++ src/Coupons.java
@@ -3,15 +3,17 @@ public class Coupons {
     public int discount;
     public String Couponsprocess(){
         if(amount>500 && amount<1000){
-            discount = amount /10;
-            System.out.println("your discount amount  : " + discount);
-            amount=amount-discount;
+            double discount;
+            discount = amount* 0.2;
+            System.out.println("your 20% discount Amount : " + discount);
+            amount= (int) (amount-discount);
             System.out.println("your final amount to pay  : "+amount);
         }
         if(amount>1000){
-            discount=amount/20;
-            System.out.println("your discount amount: "+ discount);
-            amount=amount-discount;
+            double discount;
+            discount = amount* 0.3;
+            System.out.println("your 30% discount Amount  : " + discount);
+            amount= (int) (amount-discount);
             System.out.println("your final amount to pay  : "+amount);
         }
         else{
--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
         Coupons coupons = new Coupons();
-        coupons.amount=1200;
+        coupons.amount=600;
         coupons.Couponsprocess();
 
 
