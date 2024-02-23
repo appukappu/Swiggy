3bcf58032b3c5972788ce4cffaec8433427245fb
rakesh mekala <rakeshyadavmekala15@gmail.com>
2024-02-23 15:29:25 +0530

    payment

--- src/PhonePay.java
+++ src/PhonePay.java
@@ -2,13 +2,37 @@
 
 public class PhonePay implements SwiggyPayment {
 
+    private int accountBalance;
+
+    public PhonePay(int accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
     @Override
     public String swiggypayment(int orderAmount) {
         if (accountBalance >= orderAmount) {
             int remainBal = accountBalance - orderAmount;
+            accountBalance = remainBal;
             return "Payment successful. Remaining amount: " + remainBal;
         } else {
             return "Insufficient balance";
+        }
+    }
+
+    public int getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(int accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String cancelOrder(int orderAmount) {
+        if (accountBalance >= orderAmount) {
+            accountBalance += orderAmount;
+            return "Order cancelled. Refund successful. New balance: " + accountBalance;
+        } else {
+            return "Insufficient balance for refund";
         }
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,13 @@
+
+
+
 public class Test {
 
     public static void main(String[] args) {
-        PhonePay phonePay = new PhonePay();
+        PhonePay phonePay = new PhonePay(1000);
         String result = phonePay.swiggypayment(500);
+        System.out.println(result);
+        result = phonePay.cancelOrder(500);
         System.out.println(result);
     }
 }
