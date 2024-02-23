b0db6f7227429a030ba08bda70a9917d47ea947e
chandrakala <chandraklag688@gmail.com>
2024-02-23 13:00:50 +0530

    Order items Added

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+            OrderProcess orderProcess = new OrderProcess(3,"chickenBiryani","Chicken65");
+        System.out.println(orderProcess.orderProcess());
+
+    }
+}
